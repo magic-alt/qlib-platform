73c6ffd8a618111f79833a5bc56ade33a69444c9
kaermaxc <59996146+magic-alt@users.noreply.github.com>
2026-08-16 00:20:48 +0900

    feat(research): add model explanation study (#24)

--- src/tushare_qlib/research/__init__.py
+++ src/tushare_qlib/research/__init__.py
@@ -2,16 +2,20 @@
 
 from .feature_diagnostics import FeatureDiagnosticsSpec
 from .failure_attribution import FailureAttributionSpec
+from .model_explanation import ModelExplanationSpec
 from .regime import RegimeSpec
 from .attribution_study import run_attribution_diagnose
 from .regime_study import run_regime_diagnose
+from .explanation_study import run_explanation_diagnose
 from .study import run_alpha_diagnose
 
 __all__ = [
     "FailureAttributionSpec",
     "FeatureDiagnosticsSpec",
+    "ModelExplanationSpec",
     "RegimeSpec",
     "run_alpha_diagnose",
     "run_attribution_diagnose",
+    "run_explanation_diagnose",
     "run_regime_diagnose",
 ]
