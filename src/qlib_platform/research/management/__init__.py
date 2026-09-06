bca79dba626922ae4b1214f92ad5ae05cb9f5ebc
kaermaxc <59996146+magic-alt@users.noreply.github.com>
2026-09-06 15:09:27 +0800

    feat(research): export enterprise management APIs

---
+++ src/qlib_platform/research/management/__init__.py
@@ -0,0 +1,4 @@
+from qlib_platform.research.management.governed_store import GovernedExperimentStore
+from qlib_platform.research.management.store import ResearchGovernanceStore, ResearchProject
+
+__all__ = ["GovernedExperimentStore", "ResearchGovernanceStore", "ResearchProject"]
